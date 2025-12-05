f38fa5c489afe1e8f55e706cdca38a229d2e0df3
Alex-Oliveira42 <alexsander.oliveira08@aluno.ifce.edu.br>
2025-12-05 14:49:54 -0300

    Initial Commit

---
+++ projeto_final/projeto_finalApp.swift
@@ -0,0 +1,17 @@
+//
+//  projeto_finalApp.swift
+//  projeto_final
+//
+//  Created by Found on 05/12/25.
+//
+
+import SwiftUI
+
+@main
+struct projeto_finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
