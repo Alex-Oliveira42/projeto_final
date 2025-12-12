70cbb3b272f4ebce42579e4d7bfd36ab72e96450
fernandoscheibler71 <fernando.scheibler71@aluno.ifce.edu.br>
2025-12-12 16:54:49 -0300

    Tab bar das telas

--- projeto_final/ContentView.swift
+++ projeto_final/ContentView.swift
@@ -9,16 +9,122 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            ZStack{
+                TabBar()
+            }
+            
+        }.preferredColorScheme(.dark)
+    }
+
+}
+    
+    
+struct TabBar: View {
+    var body: some View{
+        TabView{
+            HomeView()
+                .tabItem {
+                    Image(systemName: "point.bottomleft.forward.to.arrow.triangle.scurvepath.fill")
+                    Text("Home")
+                }
+            GameView()
+                .tabItem{
+                    Image(systemName: "gamecontroller")
+                    Text("Game")
+                }
+            Achievements()
+                .tabItem{
+                    Image(systemName: "trophy")
+                    Text("Achievements")
+                }
+            History()
+                .tabItem{
+                    Image(systemName: "clock.arrow.trianglehead.counterclockwise.rotate.90")
+                    Text("History")
+                }
         }
-        .padding()
+        
     }
 }
 
+struct HomeView: View{
+    var body: some View{
+        NavigationStack {
+            Text("Home")
+            
+        }
+    }
+}
+    
+    
+struct GameView: View{
+    var body: some View{
+        NavigationStack{
+            TelaGame()
+        }
+    }
+}
+
+
+struct Achievements: View {
+    var body: some View{
+        NavigationStack{
+            Text("Achievements")
+        }
+    }
+}
+
+struct History: View{
+    var body: some View{
+        NavigationStack{
+            Text("History")
+        }
+    }
+}
+
+
+
+struct TelaGame: View{
+    var body: some View{
+        VStack{
+            
+            Text("Física")
+                .italic()
+            
+            Text(" - Introdução - ")
+                .font(.system(size: 14))
+                .foregroundStyle(.gray)
+            
+            Spacer()
+        }
+        
+        
+        ScrollView{
+            VStack{
+                Text("Matéria")
+                    .foregroundStyle(.white)
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 25)
+                            .fill(Color.gray)
+                            
+                    )
+                    .stroke
+                
+                    
+                    
+                
+            }
+            
+        }
+    }
+}
+    
+    
+
+
 #Preview {
     ContentView()
+    
 }
